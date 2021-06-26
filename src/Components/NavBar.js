b57b349d7a28756ec03a7126a571354cbf4f4fa9
Riddhiagrawal001 <riddhi.agrawal2112@gmail.com>
2021-06-26 13:10:10 +0530

    added firebase

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -18,7 +18,7 @@ export default function NavBar() {
       <img
         className="nav_logo"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
-        alt="Netflix Logo"
+        alt="Netflix_Logo"
       />
       <img
         className="nav_avatar"
