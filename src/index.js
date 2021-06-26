bded4e4e2be9e1be4e4f51441bac3b4ed315750f
Riddhiagrawal001 <riddhi.agrawal2112@gmail.com>
2021-06-26 12:50:48 +0530

    initialed repo

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+// api key : 1d7e41916e0e2dfa94463b5be2701b9d
+// api sample request : https://api.themoviedb.org/3/movie/550?api_key=1d7e41916e0e2dfa94463b5be2701b9d
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ src/utils/Row.js
@@ -0,0 +1,62 @@
+import axios from "../axios/axios";
+import React from "react";
+import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = React.useState([]);
+  const [trailerUrl, setTrailerUrl] = React.useState("");
+
+  React.useEffect(() => {
+    async function fetchdata() {
+      const request = await axios.get(fetchUrl);
+      console.table(request.data.results);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchdata();
+  }, [fetchUrl]);
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row_posters">
+        {movies.map((movie) => (
+          <img
+            onClick={() => handleClick(movie)}
+            key={movie.id}
+            className={`row_poster ${isLargeRow && "row_poster_large"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
+}
+
+export default Row;
